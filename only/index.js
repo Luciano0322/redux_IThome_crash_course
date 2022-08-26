30f856a84db2654effafee7975f57c36d4b8283b
Luciano Lee <luciano.lee@webcomm.com.tw>
2022-08-26 16:20:48 +0800

    action 建立示範

--- only/index.js
+++ only/index.js
@@ -1,1 +1,22 @@
-console.log('Hello World!');
+// console.log('Hello World!');
+// 這邊都可以拆出去，但因為方便Demo，所以先不演示
+const COFFEE_ORDERED = 'COFFEE_ORDERED';
+// 建立Action
+const orderCoffee = () => {
+  return {
+    type: COFFEE_ORDERED,
+    payload: 1
+  }
+}
+// 除上述function的方法以外也可以直接帶入object
+const orderCoffeeObj = {
+  type: COFFEE_ORDERED,
+  payload: 1
+}
+// 但是我個人是不推薦這樣的做法，透過function還可以帶參數來設定，如下
+const orderCoffeeByNum = (num) => {
+  return {
+    type: COFFEE_ORDERED,
+    payload: num
+  }
+}
