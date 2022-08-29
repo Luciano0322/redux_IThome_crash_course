1d8e3c1ea94341f1bb629d787c1082cb1c50904a
Luciano Lee <luciano.lee@webcomm.com.tw>
2022-08-29 16:04:15 +0800

    整理拆分 store

--- only/index.js
+++ only/index.js
@@ -1,32 +1,6 @@
 const { orderCoffee, orderCoffeeObj, orderCoffeeByNum } = require('./action/order');
-const { reducers } = require('./reducer');
-const createStore = require('redux').createStore;
+const { store } = require('./store');
 
-// ============================================>
-// 建立Reducer
-// 先建立我們的initialState
-// const initialState = {
-//   numOfCoffee: 20,
-//   numOfCoffeeBean: 20,
-//   numOfCake: 20,
-// }
-// // 這部分和useReducer hook是一樣的
-// const reducer = (state = initialState, action) => {
-//   switch(action.type) {
-//     case COFFEE_ORDERED:
-//       return {
-//         ...state,
-//         numOfCoffee: state.numOfCoffee - action.payload
-//       }
-//     default: 
-//       return state;
-//   }
-// }
-// Reducer End============================================>
-
-// ============================================>
-// 建立Store
-const store = createStore(reducers)
 // 來試試拿取 initialState
 console.log('initial state', store.getState());
 // 當state change時可以觸發的 callback
---
+++ only/store/index.js
@@ -0,0 +1,8 @@
+const { createStore } = require("redux");
+const { reducers } = require("../reducer");
+
+const store = createStore(reducers)
+
+module.exports = {
+  store,
+}
