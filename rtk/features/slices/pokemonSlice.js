65a2068ec011608b35782813cc1c8aabbdc22244
Luciano Lee <luciano.lee@webcomm.com.tw>
2022-09-06 10:43:46 +0800

    createAsyncThunk 補完

--- rtk/features/slices/pokemonSlice.js
+++ rtk/features/slices/pokemonSlice.js
@@ -1,4 +1,5 @@
-const { createSlice } = require("@reduxjs/toolkit");
+const { createSlice, createAsyncThunk } = require("@reduxjs/toolkit");
+const axios = require('axios');
 
 // from only pokemon
 const initialState = {
@@ -7,10 +8,39 @@ const initialState = {
   error: ''
 }
 
+// 這裡我們照著 only/action/pokemon.js 裏面的邏輯重寫，使用 createAsyncThunk 就是這麼簡單
+// 我們不需要去管後續的 state change function，因為可以透過 extraReducer 來補足
+// 這裡要注意在 createAsyncThunk 第一個參數的 name 一定要對好對應的 slice name
+const fetchPokes = createAsyncThunk('pokemon/fetchPokes', (url) => {
+  return axios
+    .get(url)
+    .then(response => response.data)
+})
+
 const pokemonSlice = createSlice({
   name: 'pokemon',
   initialState,
-  reducers: {}
+  reducers: {},
+  // 這裡就按常規套路走，在 createAsyncThunk 之後可以接的有 pending, fulfilled, rejected 三種情況
+  extraReducers: builder => {
+    builder.addCase(fetchPokes.pending, state => {
+      state.loading = true
+      return state;
+    })
+    builder.addCase(fetchPokes.fulfilled, (state, action) => {
+      state.loading = false
+      state.data = action.payload
+      state.error = ''
+      return state;
+    })
+    builder.addCase(fetchPokes.rejected, (state, action) => {
+      state.loading = false
+      state.data = []
+      state.error = action.error.message
+      return state
+    })
+  }
 })
 
 module.exports = pokemonSlice.reducer;
+module.exports.fetchPokes = fetchPokes;
--- rtk/index.js
+++ rtk/index.js
@@ -2,6 +2,7 @@ const { store } = require('./features/store');
 const coffeeActions = require('./features/slices/coffeeSlice').coffeeActions;
 const coffeeBeanActions = require('./features/slices/coffeeBeanSlice').coffeeBeanActions;
 const cakeActions = require('./features/slices/cakeSlice').cakeActions;
+const fetchPokes = require('./features/slices/pokemonSlice').fetchPokes;
 
 console.log('Initial State', store.getState());
 const unsubscribe = store.subscribe(() => console.log('更新', store.getState()));
@@ -11,7 +12,8 @@ const unsubscribe = store.subscribe(() => console.log('更新', store.getState()
 // store.dispatch(coffeeActions.coffeeRestocked({qty: 10, money: 20}));
 // store.dispatch(coffeeBeanActions.coffeeBeanOrdered({qty: 2, money: 10}));
 // store.dispatch(coffeeBeanActions.coffeeBeanRestocked({qty: 5, money: 5}));
-store.dispatch(cakeActions.cakeOrdered({qty: 3, money: 45}));
-store.dispatch(cakeActions.cakeRestocked({qty: 6, money: 30}));
+// store.dispatch(cakeActions.cakeOrdered({qty: 3, money: 45}));
+// store.dispatch(cakeActions.cakeRestocked({qty: 6, money: 30}));
+store.dispatch(fetchPokes(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=20`))
 
-unsubscribe();
+// unsubscribe();
