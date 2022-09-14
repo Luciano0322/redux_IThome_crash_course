2b754500bc4995b58fd332625c6bb55331436d47
Luciano Lee <luciano.lee@webcomm.com.tw>
2022-09-14 16:13:52 +0800

    day29 內容完成

--- rtk_query/src/components/TodosList.tsx
+++ rtk_query/src/components/TodosList.tsx
@@ -1,17 +1,31 @@
-import { todoApi } from "../features/api/todoApi"
+import { useCallback } from "react";
+import { Todo, todoApi } from "../features/api/todoApi"
 
 const TodosList = () => {
   // 使用剛才的 getAll query
   const { data } = todoApi.useGetAllQuery();
+  // 這裡使用剛才的 updateTodo
+  const [ updateTodo ] = todoApi.useUpdateTodoMutation();
+  // 這裡使用剛才的 deleteTodo
+  const [ deleteTodo ] = todoApi.useDeleteTodoMutation();
+  // 這裡的話就是一般 react 的常規操作
+  const onToggle = useCallback((todo: Todo) => {
+    updateTodo({...todo, done: !todo.done})
+  }, [updateTodo])
+  // 與上述相同再操作一次
+  const onDelete = useCallback((todo: Todo) => {
+    deleteTodo(todo)
+  }, [deleteTodo])
+
   return (
     <div className="card" style={{margin: '1rem 0'}}>
       {data?.map((todo) => (
         <div className="f-b-c" key={todo.id}>
           <div>
-            <input type="checkbox" checked={todo.done} />
+            <input type="checkbox" checked={todo.done} onChange={() => onToggle(todo)}/>
             <span>{todo.text}</span>
           </div>
-          <button>delete</button>
+          <button onClick={() => onDelete(todo)}>delete</button>
         </div>
       ))}
     </div>
--- rtk_query/src/features/api/todoApi.ts
+++ rtk_query/src/features/api/todoApi.ts
@@ -15,10 +15,36 @@ export const todoApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8080/'}),
   tagTypes: ['Todos'],
   endpoints: (builder) => ({
+    // 取全部資料
     getAll: builder.query<Todo[], void>({
       // 這裡的 query 代表接續 base 要傳入的值，providesTags 就是記錄一下對應的 tagType 詳細 https://redux-toolkit.js.org/rtk-query/usage/automated-refetchin 
       query: () => `todos`,
       providesTags: [{type: 'Todos', id: 'LIST'}]
+    }),
+    // 修改 todo
+    // 這裡採用 mutation 意思是會拿取先前的 todo 然後回傳一個新的 todo 這段的概念與 React query 是相同的。
+    updateTodo: builder.mutation<Todo, Todo>({
+      // 這裡就是帶入參數的做法
+      query(todo) {
+        return {
+          url: `todos/${todo.id}`,
+          method: 'PUT',
+          body: todo,
+        };
+      },
+      // invalidatesTag 會自動去更新相對的 tagType 詳細一樣 https://redux-toolkit.js.org/rtk-query/usage/automated-refetchin
+      invalidatesTags: [{type: 'Todos', id: 'LIST'}]
+    }),
+    // 刪除功能
+    deleteTodo: builder.mutation<Todo, Todo>({
+      query(todo) {
+        return {
+          url: `todos/${todo.id}`,
+          method: 'DELETE',
+          body: todo,
+        };
+      },
+      invalidatesTags: [{type: 'Todos', id: 'LIST'}]
     })
   })
 })
